b579ae97a193b64e0e3bb7733e53ae97ab7aeb9d
Derek Butler <derekcbutler@gmail.com>
2019-09-11 21:03:42 -0600

    day2

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,6 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr){
+  return arr[2];
+}
 
 
 
@@ -48,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +72,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  let lets = [];
 
+  for(let i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+    lets.push(letters[i])
+  } return lets;
+} 
 
 
 ////////// PROBLEM 5 //////////
@@ -78,8 +94,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
+function evenFinder(nums){
+  let temp = []
+  for(let i = 0; i < nums.length; i++){
+      if( nums[i] % 2 === 0){
+         temp.push(nums[i])
+      }
+  }
+  return temp;
+  alert(temp)
+}
+evenFinder(nums);
 
 
 
